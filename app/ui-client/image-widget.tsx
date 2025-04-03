54440a342c6936ae70840986a63eb006523a08ae
mark <mmackenzie.uk@gmail.com>
2025-04-03 20:02:41 +0100

    refactor

--- app/ui-client/image-widget.tsx
+++ app/ui-client/image-widget.tsx
@@ -3,33 +3,26 @@
 import { useState } from "react";
 import { AWS_BUCKET_NAME, S3_ALBUM_NAME, SHOP } from "../configuration/wwurm";
 
+const href = `https://${AWS_BUCKET_NAME}.s3.eu-west-2.amazonaws.com/`;
+const prefix = href + S3_ALBUM_NAME + "/";
+
 export default function ImageWidget({ largeImage, smallImage }: {
     largeImage?:string;
     smallImage?: string;
 }) {
-
     const [selected, setSelected] = useState(0);
     const handleSelect = (index: number) => setSelected(index);
-
-        const thumbs = smallImage!.split(',');
-        const images = largeImage!.split(',');
-    
-        const href = `https://${AWS_BUCKET_NAME}.s3.eu-west-2.amazonaws.com/`;
-
-        console.log("thumbs ", thumbs);
-        console.log("images ", smallImage);
-        
-
+    const thumbs = smallImage!.split(',');
+    const images = largeImage!.split(',');
     return (
     <div className="product-images">
         <ul className="thumbnail-list" role="list">
         {
             thumbs.map((thumb, index) => {
-                const url = href + S3_ALBUM_NAME + "/" + thumb;  
-                console.log("url ", url, thumb)
+                const src = prefix + encodeURIComponent(thumb); 
                 return ( 
                 <li key={index} className={`thumbnail ${(index === selected) ? "thumbnail-selected" : ""}`} >
-                    <img src={url} onClick={() => handleSelect(index)} className="thumbnail-img"/>
+                    <img src={src} onClick={() => handleSelect(index)} className="thumbnail-img"/>
                 </li>)
             })    
         } 
@@ -37,12 +30,12 @@ export default function ImageWidget({ largeImage, smallImage }: {
         <ul className="images-list" role="list">
         {
             images.map((image, index) => {
-                const url = href + S3_ALBUM_NAME + "/" + encodeURIComponent(image);  
+                const src = prefix + encodeURIComponent(image);  
                 return (
                 <li key={index} className="product-image-wrap">
                     <img 
                         className = {`product-image ${(index === selected) ? "selected" : ""}`} 
-                        src={url} 
+                        src={src} 
                     />
                 </li>)
             })           
